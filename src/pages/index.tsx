74a012129cfc48c9e3b2a0760c949fff54db1f2b
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-02-14 16:37:32 +0300

    Initial project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+//NextJS import
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>BAU Time</title>
+        <meta name="description" content="A portal for BAU units" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
