fb918de1183f8827605ea02d19db504629900c2a
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-04-03 21:35:31 +0300

    ⚙️ Functionality : Added mark as read functionality

--- src/components/notificationModal/notificationModal.tsx
+++ src/components/notificationModal/notificationModal.tsx
@@ -16,6 +16,9 @@ import { motion, AnimatePresence } from "framer-motion";
 import { notifcationModalAnimation } from "../../utils/page-transition";
 import CustomImage from "../customImage/customImage";
 
+//supabase import
+import supabase from "../../utils/supabase";
+
 const NotificationModal: FunctionComponent = () => {
   //Recoil state
   const isModalOpen = useRecoilValue(notificationModalState);
@@ -46,17 +49,28 @@ const NotificationModal: FunctionComponent = () => {
           variants={notifcationModalAnimation}
           className="absolute right-[5rem] z-20 top-[5rem] w-[20rem] max-h-[25rem] overflow-x-hidden overflow-y-auto bg-white rounded-md shadow-lg  dark:bg-gray-800"
         >
-          <div className="py-2 flex flex-col flex-wrap max-w-[10rem] static">
+          <div className="py-2 flex flex-col flex-wrap max-w-[10rem] static cursor-pointer">
             {myNotifications.map((notification) => {
               const sender = employees.find(
                 (employee) => employee.id === notification.sender
               );
               return (
                 <div
+                  onClick={async () => {
+                    if (notification.isRead) return;
+                    const { data, error } = await supabase
+                      .from("notifications")
+                      .update({ isRead: true })
+                      .match({
+                        id: notification.id,
+                      });
+
+                    if (error) console.log(error);
+                  }}
                   key={notification.id}
-                  className={
-                    `flex flex-col flex-wrap  gap-y-2 p-2 w-[20rem] border-b border-gray-700 ${!notification.isRead && "bg-bt-dark-gray" } `
-                  }
+                  className={`flex flex-col flex-wrap  gap-y-2 p-2 w-[20rem] border-b border-gray-700 ${
+                    !notification.isRead && "bg-bt-dark-gray"
+                  } `}
                 >
                   <div className="flex gap-x-2 items-center">
                     <div className="relative w-[2.5rem] h-[2.5rem] rounded-full">
@@ -71,7 +85,6 @@ const NotificationModal: FunctionComponent = () => {
                     </div>
 
                     <span>{sender?.name}</span>
-
                   </div>
                   <span>{notification.message}</span>
                 </div>
@@ -80,9 +93,22 @@ const NotificationModal: FunctionComponent = () => {
           </div>
           <a
             href="#"
+            onClick={async () => {
+              myNotifications.forEach(async (notification) => {
+                if(notification.isRead) return;
+                const { data, error } = await supabase
+                  .from("notifications")
+                  .update({ isRead: true })
+                  .match({
+                    id: notification.id,
+                  });
+
+                if (error) console.log(error);
+              });
+            }}
             className="block sticky bottom-0 py-2 font-bold text-center text-white bg-gray-800 dark:bg-gray-700 hover:underline"
           >
-            See all notifications
+            Mark All As Read
           </a>
         </motion.div>
       )}
