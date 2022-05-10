2380ef31cb58529e10441af13f29b95c06e9f631
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-05-10 23:28:29 +0300

    🔥 Hotfix : typescript update

--- next.config.js
+++ next.config.js
@@ -5,8 +5,17 @@ const withTM = require("next-transpile-modules")(["gsap"]);
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.pexels.com" , "yt3.ggpht.com" , "media-exp1.licdn.com" , "www.helixhelezon-courses.com" , "pbs.twimg.com" , "images.pexels.com"],
-    
+    domains: [
+      "images.pexels.com",
+      "yt3.ggpht.com",
+      "media-exp1.licdn.com",
+      "www.helixhelezon-courses.com",
+      "pbs.twimg.com",
+      "images.pexels.com",
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
